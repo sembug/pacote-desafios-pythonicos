79ce68642b3690625b7bb69b064d13799d3d3c89
Roberto Nunes <roberto@krooze.com.br>
2020-10-08 12:41:12 -0300

    Solucao desafio 04

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -13,8 +13,7 @@ Assuma que a e b tem tamanho 2 ou maior.
 """
 
 def mix_up(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    return "{0}{1} {2}{3}".format(b[:2], a[2:], a[:2], b[2:])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
