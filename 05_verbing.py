fe418f690ec5b7a7a26bdfbd12c8a1f876e5c7d5
Roberto Nunes <roberto@krooze.com.br>
2020-10-08 12:49:04 -0300

    Solucao desafio 05

--- 05_verbing.py
+++ 05_verbing.py
@@ -11,8 +11,13 @@ Retorne o resultado da string.
 """
 
 def verbing(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) < 3:
+        return s
+
+    if s.endswith('ing'):
+        return s + 'ly'
+    else:
+        return s + 'ing'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
