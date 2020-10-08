0d8f55ea2288a8849db73c69d0d8a756735d8309
Roberto Nunes <roberto@krooze.com.br>
2020-10-08 14:15:19 -0300

    Solucao desafio 07

--- 07_front_back.py
+++ 07_front_back.py
@@ -10,9 +10,18 @@ Exemplo: 'abcde', a metade da frente é 'abc' e a de trás é 'de'.
 Finalmente, dadas duas strings a e b, retorne uma string na forma:
 a-frente + b-frente + a-trás + b-trás
 """
+def split(s):
+    if len(s) % 2 == 0:
+        index = len(s) // 2
+    else:
+        index = len(s) // 2 + 1   
+    return (s[:index], s[index:])
+
+
 def front_back(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    s_a = split(a)
+    s_b = split(b)
+    return "{0}{1}{2}{3}".format(s_a[0], s_b[0], s_a[1], s_b[1])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
